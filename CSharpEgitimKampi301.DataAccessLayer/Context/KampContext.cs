5f47f753a028b62c4d186541c43466072f7876fa
EbruTerhan <terhanebru883@gmail.com>
2025-11-13 15:34:08 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.DataAccessLayer/Context/KampContext.cs
@@ -0,0 +1,24 @@
+﻿using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Context
+{
+    public class KampContext : DbContext
+    {
+        public DbSet<Category> categories  { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+
+    }
+
+}
